e823199566ce3fa0e5e5194bef19626a2ff2c4fe
The Cheaterman <the.cheaterman@gmail.com>
2019-04-17 14:30:38 +0200

    Quote WSGI_MODULE env variable in case of spaces

--- run.sh
+++ run.sh
@@ -8,5 +8,5 @@ if [[ "$1" == "bash" ]]
 then
     bash
 else
-    uwsgi --virtualenv /env --chdir /code -w ${WSGI_MODULE:-wsgi:app} --uwsgi-socket 0.0.0.0:5000
+    uwsgi --virtualenv /env --chdir /code -w "${WSGI_MODULE:-wsgi:app}" --uwsgi-socket 0.0.0.0:5000
 fi
